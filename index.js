ba6016717596ef198abdc18e76597ad26a0550ec
Nicholas Hubbard <hubbardn95@gmail.com>
2014-11-25 16:39:56 -0500

    Updated Methods
    * Replaced Get Method with @ninjabiscuit's method
    (madebymany/sir-trevor-js#255, comment #8)
    * Commented out Replace Method due to Re-initialization method issues

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var $ = require('jquery-browserify');
 (function ($) {
 	$.fn.stGetJSON = function() {
-		this.text();
-		return this;
+		return SirTrevor.getInstance(0).store("read");
 	}
 	$.fn.stReplaceJSON = function(json) {
-		this.text(json);
+		// this.text(json);
+		// Currently, this is commented out due to re-initialization issues on Sir Trevor.
+		// This is an in-progress effort between @ninjabiscuit (Sir Trevor expert) and me (@nhubbard) on GitHub. Follow our progress with madebymany/sir-trevor-js#255.
 	}
 }( jQuery ));
